ba8f7409ddaf806a54433886e8f8fbed63a0a4c5
Jonathan Frederick Jay <jonathan.jay10@gmail.com>
2017-06-08 12:13:45 -0700

    Moved all styling to a stylesheet.

--- src/components/HabitCard/HabitCard.js
+++ src/components/HabitCard/HabitCard.js
@@ -5,134 +5,60 @@ import {
     View, 
     Text,
     Image,
-    AppRegistry
+    StyleSheet
 } from 'react-native';
 
 export default class HabitCard extends Component {
     render () {
         console.log(this.props.picture);
         return (
-            <View style={{                  // Main Container
-                flex: 1,
-                flexDirection: 'column',
-                backgroundColor: 'white',
-                alignSelf: 'stretch',
-                marginTop: 15,
-                marginLeft: 15,
-                marginRight: 15,
-                padding: 15,
-                borderColor: '#EEE',
-                borderLeftColor: '#FFF',
-                borderTopColor: '#FFF',
-                borderStyle: 'solid',
-                borderBottomWidth: 3,
-                borderLeftWidth: 1,
-                borderRightWidth: 1,
-                borderTopWidth: 1,
-                borderRadius: 5
-            }}>
-                <View style={{                      //Top container (Img, title, and info)
-                    flex: 10,
-                    flexDirection: 'row',
-                    marginBottom: 10
-                }}>
-                    <View style={{                  //IMG container
-                        flex: 3,
-                        alignItems: 'center',
-                        marginBottom: -10,
-                    }}>
+            <View style={ styles.mainContainer }>
+                <View style={ styles.topContainer }>
+                    <View style={ styles.imageContainer }>
                         <Image 
-                            style={{
-                                    width: 100, 
-                                    height: 120
-                            }}
+                            style={ styles.picture }
                             source={{ uri: "http://" + config.ip + ":" + config.port + "/pics/" + this.props.picture }}
                         />
                     </View>
 
-                    <View style={{                  //Top right container (title, info)
-                        flex: 4,
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}>
-                        <View style={{                  //Title container
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'flex-end'
-                        }}>
-                            <Text style={{              // Title
-                                flex: 1,
-                                borderStyle: 'solid',
-                                borderBottomColor: '#CFCFCF',
-                                borderBottomWidth: 1,
-                                marginBottom: 15
-                            }}> 
+                    <View style={ styles.topRightContainer}>
+                        <View style={ styles.titleContainer }>
+                            <Text 
+                                style={ styles.title }> 
                                 {this.props.title}
                             </Text>
-                            <Text style={{              //Info
-                                flex: 3,
-                                textAlign: 'center',
-                                padding: 10,
-                                borderColor: '#CFCFCF',
-                                borderTopWidth: 1,
-                                borderRightWidth: 2,
-                                borderLeftWidth: 2,
-                                borderBottomWidth: 3
-                            }}>
+                            <Text style={ styles.info }>
                                 {this.props.info}
                             </Text>
                         </View>
                     </View>
                 </View>
 
-                <ProgressBar
-                     accentColor={this.props.accentColor}
-                />
+                <ProgressBar accentColor={this.props.accentColor} />
 
-                <View style={{                          //Bottom Container (notes, icons, streak)
-                    flex: 3,
-                    flexDirection: 'row',
-                    marginTop: 10
-                }}>
+                <View style={ styles.bottomContainer }>
 
-                    <Text style={{                      //Notes
-                        marginLeft: 15,
-                        flex: 1,
-                    }}>
+                    <Text style={ styles.notes }>
                         Notes will go here.
                     </Text>
 
-                    <View style={{                      //Icons
-                        flex: 1,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        marginRight: 15
-                    }}>
+                    <View style={ styles.iconContainer }>
 
-                        <Image source={require('./edit.png')}
-                            style={{
-                                width: 20, 
-                                height: 20
-                            }}
+                        <Image 
+                            source={require('./edit.png')}
+                            style={ styles.icon }
                         />
-                        <Image source={require('./heart.png')}
-                            style={{
-                                width: 20,
-                                height: 20
-                            }}
+                        <Image 
+                            source={require('./heart.png')}
+                            style={ styles.icon }
                         />
-                        <Image source={require('./share.png')}
-                            style={{
-                                width: 20, 
-                                height: 20
-                            }}
+                        <Image 
+                            source={require('./share.png')}
+                            style={ styles.icon }
                         />
-                        <Image source={require('./notification.png')}
-                            style={{
-                                width: 20, 
-                                height: 20
-                            }}
+                        <Image 
+                            source={require('./notification.png')}
+                            style={ styles.icon }
                         />
                     </View>
 
@@ -141,3 +67,86 @@ export default class HabitCard extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        alignSelf: 'stretch',
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 15,
+        padding: 15,
+        borderColor: '#EEE',
+        borderLeftColor: '#FFF',
+        borderTopColor: '#FFF',
+        borderStyle: 'solid',
+        borderBottomWidth: 3,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        borderRadius: 5
+    },
+    topContainer: {
+        flex: 10,
+        flexDirection: 'row',
+        marginBottom: 10
+    },
+    imageContainer: {
+        flex: 3,
+        alignItems: 'center',
+        marginBottom: -10,
+    },
+    picture: {
+            width: 100, 
+            height: 120
+    },
+    topRightContainer: {
+        flex: 4,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    titleContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
+    title: {
+        flex: 1,
+        borderStyle: 'solid',
+        borderBottomColor: '#CFCFCF',
+        borderBottomWidth: 1,
+        marginBottom: 15
+    },
+    info: {
+        flex: 3,
+        textAlign: 'center',
+        padding: 10,
+        borderColor: '#CFCFCF',
+        borderTopWidth: 1,
+        borderRightWidth: 2,
+        borderLeftWidth: 2,
+        borderBottomWidth: 3
+    },
+    bottomContainer: {
+        flex: 3,
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    notes: {
+        marginLeft: 15,
+        flex: 1,
+    },
+    iconContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginRight: 15
+    },
+    icon: {
+        width: 20, 
+        height: 20
+    }
+});
