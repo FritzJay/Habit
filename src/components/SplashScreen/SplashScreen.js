119ef1169fbacd631793ed297a5c4a7776bd7b78
Jonathan Frederick Jay <jonathan.jay10@gmail.com>
2017-08-22 06:07:16 -0700

    Added text and circles to splash screen.

--- src/components/SplashScreen/SplashScreen.js
+++ src/components/SplashScreen/SplashScreen.js
@@ -21,15 +21,71 @@ export default class Register extends React.Component {
                     position: 'absolute',
                     width: windowWidth,
                     height: windowHeight,
-                    backgroundColor: 'white'
+                    backgroundColor: 'white',
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
                 }}
             duration= {500}
             >
-                <Text>TESTTESTTEST</Text>
+                <Text 
+                    style={ styles.title }
+                >
+                    Habit
+                </Text>
+                <View
+                    style={ styles.circlesContainer }
+                >
+                    <View
+                        style={[
+                            styles.circle,
+                            styles.circleOne 
+                        ]}
+                    />
+                    <View
+                        style={[
+                            styles.circle,
+                            styles.circleTwo 
+                        ]}
+                    />
+                    <View
+                        style={[
+                            styles.circle,
+                            styles.circleThree 
+                        ]}
+                    />
+                </View>
             </FadeInView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 51,
+        color: '#172E58',
+        textAlign: 'center'
+    },
+    circlesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center', 
+    },
+    circle: {
+        width: 35,
+        height: 35,
+        borderRadius: 29,
+        margin: 5,
+    },
+    circleOne: {
+        backgroundColor: '#E84721',
+    },
+    circleTwo: {
+        backgroundColor: '#F9BE01',
+    },
+    circleThree: {
+        backgroundColor: '#0595C2',
+    },
+})
 
 const windowWidth = Dimensions.get('window').width,
   windowHeight = Dimensions.get('window').height
